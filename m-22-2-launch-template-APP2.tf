2a009a7d9e92294ca1c957be39825d6b862d0797
lika090909 <likulya157@gmail.com>
2025-09-05 09:18:17 -0700

    saving before trying ecs

--- m-22-2-launch-template-APP2.tf
+++ m-22-2-launch-template-APP2.tf
@@ -1,57 +1,57 @@
-resource "aws_launch_template" "myec2_launch_template-app2" {
+# resource "aws_launch_template" "myec2_launch_template-app2" {
   
-  name = "${var.environment}-EC2-APP2"
+#   name = "${var.environment}-EC2-APP2"
 
-  user_data           = filebase64("${path.module}/APP-2.sh")
-  instance_type = var.instance_type
-  key_name = var.private-ec2-keypair
-  vpc_security_group_ids = [module.security-group_private.security_group_id]
+#   user_data           = filebase64("${path.module}/APP-2.sh")
+#   instance_type = var.instance_type
+#   key_name = var.private-ec2-keypair
+#   vpc_security_group_ids = [module.security-group_private.security_group_id]
 
-  update_default_version = true 
+#   update_default_version = true 
 
-  block_device_mappings {
-    device_name = "/dev/sdf"
+#   block_device_mappings {
+#     device_name = "/dev/sdf"
 
-    ebs {
-      volume_size = 10
-    }
-  }
-
-#   capacity_reservation_specification {
-#     capacity_reservation_preference = "open"
+#     ebs {
+#       volume_size = 10
+#     }
 #   }
 
-  cpu_options {
-    core_count       = 1
-    threads_per_core = 2
-  }
+# #   capacity_reservation_specification {
+# #     capacity_reservation_preference = "open"
+# #   }
 
-  credit_specification {
-    cpu_credits = "standard"
-  }
+#   cpu_options {
+#     core_count       = 1
+#     threads_per_core = 2
+#   }
 
-  disable_api_stop        =  false
-  disable_api_termination = false
+#   credit_specification {
+#     cpu_credits = "standard"
+#   }
 
-  ebs_optimized = true
+#   disable_api_stop        =  false
+#   disable_api_termination = false
 
-  image_id =  data.aws_ami.amz-2023.id
+#   ebs_optimized = true
 
-  instance_initiated_shutdown_behavior = "terminate"
+#   image_id =  data.aws_ami.amz-2023.id
 
-  
-
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "enabled"
-  }
-
-  monitoring {
-    enabled = true
-  }
-  tags = local.common_tags
+#   instance_initiated_shutdown_behavior = "terminate"
 
   
-}
+
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_tokens                 = "required"
+#     http_put_response_hop_limit = 1
+#     instance_metadata_tags      = "enabled"
+#   }
+
+#   monitoring {
+#     enabled = true
+#   }
+#   tags = local.common_tags
+
+  
+# }
